1894a419888b7bf6aba3be5b6b8d707f3f142855
esaacol <sni.sanchezjavier@gmail.com>
2026-02-12 18:31:46 -0400

    Refactor database connection parameters and error handling

--- config.php
+++ config.php
@@ -1,18 +1,15 @@
 <?php
-
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+session_start();
 
 $host = getenv("DB_HOST");
-$dbname = getenv("DB_NAME");
-$user = getenv("DB_USER");
-$password = getenv("DB_PASS");
 $port = getenv("DB_PORT");
+$db   = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
 
 try {
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Error de conexión");
+    die("Error de conexión: " . $e->getMessage());
 }
