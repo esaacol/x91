7b299328d725bc37a997fe64ea0b71894fce4d91
esaacol <sni.sanchezjavier@gmail.com>
2026-02-12 12:09:27 -0400

    Refactor database connection to use environment variables

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
-$host = "dpg-d66krl06fj8s7397t3lg-a.oregon-postgres.render.com";
-$dbname = "x91_db";
-$user = "x91_db_user";
-$password = "at26s0tdPHR6rb9ckPotpNLIMJoypc61";
-$port = "5432";
+session_start();
+
+$host = getenv("DB_HOST");
+$dbname = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASS");
+$port = getenv("DB_PORT");
 
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa";
 } catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
+    die("Error de conexión");
 }
-?>
